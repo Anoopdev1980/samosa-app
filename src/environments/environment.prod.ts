4dce68a5406e692e33e4b3a69a3900d821f16ba5
Anoopdev1980 <kumar.anoop1980@gmail.com>
2023-08-05 22:31:08 +0530

    API path change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,6 +17,7 @@ export const environment = {
  baseUrl: 'https://samosanmore.in',
 };
 
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
